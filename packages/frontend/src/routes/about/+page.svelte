0d93b229a01518638fe58ceccea71cb961b6e314
Netsyntaxdev <143629083+Netsyntaxdev@users.noreply.github.com>
2023-08-30 16:16:37 +0100

    Update +page.svelte

--- packages/frontend/src/routes/about/+page.svelte
+++ packages/frontend/src/routes/about/+page.svelte
@@ -11,57 +11,28 @@
 	<h1>About</h1>
 
 	<p>
-		<i>cryptgeon</i> is a secure, open source sharing note / file service inspired by
-		<a href="https://privnote.com"><i>PrivNote</i></a>.
+		<i>crypnote</i> es un servicio seguro y de código abierto para compartir notas/archivos.
 	</p>
 
-	<AboutParagraph title="how does it work?">
+	<AboutParagraph title="Como funciona?">
 		<span>
-			each note has a generated <code>id (256bit)</code> and <code>key 256(bit)</code>. The
+			cada nota tiene un generado <code>id (256bit)</code> y <code>key 256(bit)</code>. El
 			<code>id</code>
-			is used to save & retrieve the note. the note is then encrypted with aes in gcm mode on the client
-			side with the <code>key</code> and then sent to the server. data is stored in memory and never
-			persisted to disk. the server never sees the encryption key and cannot decrypt the contents of
-			the notes even if it tried to.
+			El id se utiliza para guardar y recuperar la nota. Luego, la nota se cifra con aes en modo gcm
+			en el lado del cliente <code>key</code> luego se envía al servidor. los datos se almacenan en la 
+			memoria y nunca persisten en el disco. el servidor nunca ve la clave de cifrado y no puede  
+			descifrar el contenidode las notas
 		</span>
 	</AboutParagraph>
 
 	<AboutParagraph title="features">
 		<ul>
-			<li>server cannot decrypt contents due to client side encryption</li>
-			<li>view and time constraints</li>
-			<li>in memory, no persistence</li>
+			<li>El servidor no puede descifrar el contenido debido al cifrado del lado del cliente.</li>
+			<li>Limitaciones de tiempo y vista</li>
+			<li>En la memoria, sin persistencia</li>
 		</ul>
 	</AboutParagraph>
 
-	<AboutParagraph title="tech stack">
-		<span>
-			the backend is written in rust and the frontend is svelte and typescript.
-			<br />
-			you are welcomed to check & audit the
-			<a href="https://github.com/cupcakearmy/cryptgeon" target="_blank" rel="noopener noreferrer">
-				source code
-			</a>.
-		</span>
-	</AboutParagraph>
-
-	<AboutParagraph title="translations">
-		<span
-			>translations are managed on <a
-				href="https://lokalise.com/"
-				target="_blank"
-				rel="noopener noreferrer">Lokalise</a
-			>, which granted an open source license to use the paid version. If you are interested in
-			helping translating don't hesitate to contact me!
-		</span>
-	</AboutParagraph>
-
-	<AboutParagraph title="attribution">
-		<span>
-			icons made by <a href="https://www.freepik.com" title="Freepik">freepik</a> from
-			<a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-		</span>
-	</AboutParagraph>
 
 	<AboutParagraph title="version">
 		<span>
